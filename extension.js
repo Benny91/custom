e9dc9e02332d7c8800f69517e4a45b1005970354
Benny91 <benpaul91@gmail.com>
2016-06-25 18:50:42 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 (function () {
 
     // Change this to your GitHub username so you don't have to modify so many things.
-    var fork = "basicBot";
+    var fork = "Benny91";
 
     // Define our function responsible for extending the bot.
     function extend() {
@@ -46,10 +46,10 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-      botName: "basicBot",
+      botName: "ChillinBot",
       language: "english",
-      chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
-      scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+      chatLink: "https://rawgit.com/Benny91/source/master/lang/en.json",
+      scriptLink: "https://rawgit.com/Benny91/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
       startupVolume: 0, // 0-100
@@ -105,13 +105,13 @@
       songstats: true,
       commandLiteral: "!",
       blacklists: {
-        NSFW: "https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json",
-        OP: "https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json",
-        BANNED: "https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json"
+        NSFW: "https://rawgit.com/Benny91/custom/master/blacklists/NSFWlist.json",
+        OP: "https://rawgit.com/Benny91/custom/master/blacklists/OPlist.json",
+        BANNED: "https://rawgit.com/Benny91/custom/master/blacklists/BANNEDlist.json"
       }
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/basicBot/source/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/Benny91/source/master/basicBot.js", extend);
 
 }).call(this);
